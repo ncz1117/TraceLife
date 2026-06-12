a0e30693e00e032b3c4a4e5f6bb492fd1bce1aad
ncz1117 <nongchangzhu1117@gmail.com>
2026-06-12 17:42:37 +0800

    fix: APK build - gradle.properties 加 Kotlin daemon 修复 + 禁 parallel + 禁 incremental

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,9 +8,11 @@ plugins {
 android {
     namespace = "com.tracelife.trace_life"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"  // plugins require this version
 
     compileOptions {
+        // Core library desugaring required by flutter_local_notifications
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -24,10 +26,11 @@ android {
         applicationId = "com.tracelife.trace_life"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21  // flutter_local_notifications requires >= 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -41,4 +44,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
